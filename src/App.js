0f17907669fda5b3a63029cc0a20dd958874cbbc
Solomon <solomon@combyne.com>
2023-01-18 18:46:47 +0100

    initial setup

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css'
+
+import { Suspense } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import Overview from './pages/Customer/Overview/Overview'
+
+function App() {
+  return (
+    <Router>
+      <Suspense fallback="loading">
+        <Routes>
+          <Route path="/" element={<Overview />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  )
+}
+
+export default App
