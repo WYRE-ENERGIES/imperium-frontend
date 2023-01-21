860366bcb36905f2b3a1c07a3f6003c96f16c4dd
Solomon <solomon@combyne.com>
2023-01-21 14:02:00 +0100

    design overview widget

---
+++ src/components/SHSTable/SHSTable.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SHSTable = () => {
+  return <div>SHSTable</div>
+}
+
+export default SHSTable
--- src/pages/Customer/Overview/Overview.jsx
+++ src/pages/Customer/Overview/Overview.jsx
@@ -1,7 +1,11 @@
+import { ReactComponent as EnergyWidgetIcon } from '../../../assets/overview/energy-icon.svg'
+import { ReactComponent as HomeWidgetIcon } from '../../../assets/overview/home-icon.svg'
 import InstructionModal from './InstructionModal/InstructionModal'
+import OverviewWidget from '../../../components/Widget/Overview/OverviewWidget'
 import PageBreadcrumb from '../../../components/PageBreadcrumb/PageBreadcrumb'
 import PageLayout from '../../../components/Layout/PageLayout'
 import React from 'react'
+import { ReactComponent as SEnergyWidgetIcon } from '../../../assets/overview/cancel-energy-con.svg'
 import classes from './Overview.module.scss'
 import { theme } from 'antd'
 
@@ -10,10 +14,45 @@ const Overview = () => {
     token: { colorBgContainer },
   } = theme.useToken()
 
+  const widgets = [
+    {
+      id: 1,
+      icon: EnergyWidgetIcon,
+      title: 'Total Energy Generation ',
+      range: 'For the year',
+      value: '100.241',
+    },
+    {
+      id: 2,
+      icon: SEnergyWidgetIcon,
+      title: 'Total Energy Consumption',
+      range: 'For the year',
+      value: '50.82',
+    },
+    {
+      id: 3,
+      icon: HomeWidgetIcon,
+      title: 'Total SHS',
+      range: 'For the year',
+      value: '7',
+    },
+  ].map((widget) => (
+    <OverviewWidget
+      key={widget.id}
+      Icon={widget.icon}
+      range={widget.range}
+      title={widget.title}
+      value={widget.value}
+    />
+  ))
+
   return (
     <PageLayout>
-      <div style={{ backgroundColor: colorBgContainer }}>Overview Page</div>
-      <InstructionModal />
+      <div style={{ backgroundColor: colorBgContainer }}>
+        Overview Page
+        <div className={classes.Overview__widgets}>{widgets}</div>
+      </div>
+      {/* <InstructionModal /> */}
     </PageLayout>
   )
 }
