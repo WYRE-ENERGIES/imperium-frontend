5a2694e8746665044e171ca649a8b90ddd0a7d66
evie-adenoyin <ekong.emmanuel@lmu.edu.ng>
2023-01-23 00:29:59 +0100

    upload file form input

--- src/components/Form/businessForm.jsx
+++ src/components/Form/businessForm.jsx
@@ -1,5 +1,8 @@
-import { Button, Form, Input } from 'antd'
+// import { InboxOutlined } from '@ant-design/icons'
+import { Button, Form, Input, Upload } from 'antd'
 import { useNavigate } from 'react-router-dom'
+
+import uploadImg from '../../assets/images/Auth/Featured icon.svg'
 
 const BusinessForm = () => {
   const nextPage = useNavigate()
@@ -13,6 +16,13 @@ const BusinessForm = () => {
   }
   const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo)
+  }
+  const normFile = (e) => {
+    console.log('Upload event:', e)
+    if (Array.isArray(e)) {
+      return e
+    }
+    return e?.fileList
   }
 
   return (
@@ -52,6 +62,8 @@ const BusinessForm = () => {
                 <span
                   style={{
                     color: '#c4c4c4',
+                    borderRight: '1px solid #C4C4C4',
+                    paddingRight: '20px',
                   }}
                 >
                   {'https://'}
@@ -60,12 +72,32 @@ const BusinessForm = () => {
               placeholder="Enter your last name"
             />
           </Form.Item>
-          <Form.Item
-            label={<p style={{ marginBottom: '-30px' }}>Upload Company Logo</p>}
-            name="address"
-            required
-          >
-            <Input.TextArea placeholder="Enter a Address..." />
+          <Form.Item label="Upload Compnay Logo">
+            <Form.Item
+              name="dragger"
+              valuePropName="fileList"
+              getValueFromEvent={normFile}
+              noStyle
+            >
+              <Upload.Dragger name="files" action="/upload.do">
+                <p className="ant-upload-drag-icon">
+                  <img src={uploadImg} alt="" srcSet="" />
+                </p>
+                <p
+                  className="ant-upload-text"
+                  style={{ color: '#808080', fontSize: '14px' }}
+                >
+                  <strong style={{ color: '#385E2B' }}>Click to upload</strong>{' '}
+                  or drag and drop
+                </p>
+                <p
+                  className="ant-upload-hint"
+                  style={{ color: '#808080', fontSize: '12px' }}
+                >
+                  SVG, PNG, JPG or GIF (max. 800x400px)
+                </p>
+              </Upload.Dragger>
+            </Form.Item>
           </Form.Item>
 
           <Form.Item>
