a025a635de5a5953a4127e23a9d6a9ad1b7fe876
evie-adenoyin <ekong.emmanuel@lmu.edu.ng>
2023-01-25 22:42:58 +0100

    Page routing configuration

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ import Overview from './pages/Customer/Overview/Overview'
 import { Support } from './pages/Customer/Support/Support'
 import { Suspense } from 'react'
 
+import SignUp from './pages/Customer/Auth/SignUp/SignUp'
+import SignIn from './pages/Customer/Auth/SignIn/SignIn'
+import Verification from './pages/Customer/Auth/Verification/Verification'
+import Detail from './pages/Customer/Auth/Detail/Detail'
+import ForgotPasswordPage from './pages/Customer/Auth/ForgotPassword/ForgotPassword'
+
 function App() {
   return (
     <Router>
@@ -19,6 +25,11 @@ function App() {
           </Route>
           <Route path="/support" element={<Support />} />
           <Route path="/energy-analytic" element={<EnergyAnalytic />} />
+          <Route path="/" element={<SignUp />} />
+          <Route path="signin" element={<SignIn />} />
+          <Route path="verification/code" element={<Verification />} />
+          <Route path="details" element={<Detail />} />
+          <Route path="forgot/password/" element={<ForgotPasswordPage />} />
         </Routes>
       </Suspense>
     </Router>
