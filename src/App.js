735d2a3e2deed9119e8d21d259bc8cc7480479e9
Solomon <solomon@combyne.com>
2023-02-05 22:40:47 +0100

    add lazy loading for optimization

--- src/App.js
+++ src/App.js
@@ -3,22 +3,38 @@ import 'react-toastify/dist/ReactToastify.css'
 
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
-import Battery from './pages/Customer/Battery/Battery'
-import Business from './pages/Customer/Auth/Business/Business'
-import Detail from './pages/Customer/Auth/Detail/Detail'
-import DisabledAccount from './pages/Customer/Error/DisabledAccount/DisabledAccount'
-import ErrorContact from './pages/Customer/Error/ErrorContact/ErrorContact'
-import EnergyAnalytic from './pages/Customer/EnergyAnalytic/EnergyAnalytic'
-import ForgotPasswordPage from './pages/Customer/Auth/ForgotPassword/ForgotPassword'
-import NewPasswordPage from './pages/Customer/Auth/NewPassword/Newpassword'
-import OTP from './pages/Customer/Auth/Otp/Otp'
-import Overview from './pages/Customer/Overview/Overview'
-import PanelAnalytic from './pages/Customer/PanelAnalytic/PanelAnalytic'
-import SignIn from './pages/Customer/Auth/SignIn/SignIn'
-import SignUp from './pages/Customer/Auth/SignUp/SignUp'
-import Support from './pages/Customer/Support/Support'
 import { Suspense } from 'react'
-import Verification from './pages/Customer/Auth/Verification/Verification'
+import { lazy } from 'react'
+
+const Battery = lazy(() => import('./pages/Customer/Battery/Battery'))
+const Business = lazy(() => import('./pages/Customer/Auth/Business/Business'))
+const Detail = lazy(() => import('./pages/Customer/Auth/Detail/Detail'))
+const DisabledAccount = lazy(() =>
+  import('./pages/Customer/Error/DisabledAccount/DisabledAccount'),
+)
+const ErrorContact = lazy(() =>
+  import('./pages/Customer/Error/ErrorContact/ErrorContact'),
+)
+const EnergyAnalytic = lazy(() =>
+  import('./pages/Customer/EnergyAnalytic/EnergyAnalytic'),
+)
+const ForgotPasswordPage = lazy(() =>
+  import('./pages/Customer/Auth/ForgotPassword/ForgotPassword'),
+)
+const NewPasswordPage = lazy(() =>
+  import('./pages/Customer/Auth/NewPassword/Newpassword'),
+)
+const OTP = lazy(() => import('./pages/Customer/Auth/Otp/Otp'))
+const Overview = lazy(() => import('./pages/Customer/Overview/Overview'))
+const PanelAnalytic = lazy(() =>
+  import('./pages/Customer/PanelAnalytic/PanelAnalytic'),
+)
+const SignIn = lazy(() => import('./pages/Customer/Auth/SignIn/SignIn'))
+const SignUp = lazy(() => import('./pages/Customer/Auth/SignUp/SignUp'))
+const Support = lazy(() => import('./pages/Customer/Support/Support'))
+const Verification = lazy(() =>
+  import('./pages/Customer/Auth/Verification/Verification'),
+)
 
 function App() {
   return (
