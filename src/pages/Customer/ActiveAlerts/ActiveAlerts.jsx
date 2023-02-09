5844cbaece7fea2d1166ef9bdb58b5f4bd3ddfbf
evie-adenoyin <ekong.emmanuel@lmu.edu.ng>
2023-02-09 10:59:11 +0100

    Active alert table design

--- src/pages/Customer/ActiveAlerts/ActiveAlerts.jsx
+++ src/pages/Customer/ActiveAlerts/ActiveAlerts.jsx
@@ -1,12 +1,94 @@
 import React from 'react'
 import PageLayout from '../../../components/Layout/PageLayout'
+import { Table } from 'antd'
 import PageBreadcrumb from '../../../components/PageBreadcrumb/PageBreadcrumb'
 import ShsCapacityDropdown from '../../../components/ShsCapacityDropdown/ShsCapacityDropdown'
 import classes from './ActiveAlerts.module.scss'
+import FormButton from '../../../components/Auth/Forms/Widgets/FormButton'
 const ActiveAlerts = () => {
+  const title = () => (
+    <p style={{ fontWeight: '500', fontSize: '18px' }}>Active Alerts Table</p>
+  )
+  const footer = () => {
+    return (
+      <div className={classes.ActiveAlerts__Footer}>
+        <div className={classes.ActiveAlerts__NavBtn}>
+          {' '}
+          <FormButton type="button" action="Previous" />
+          <FormButton type="button" action="Next" />
+        </div>
+        <div className={classes.ActiveAlerts__Pagination}>Page 1 of 10</div>
+      </div>
+    )
+  }
+  const columns = [
+    {
+      title: 'Active Alert',
+      dataIndex: 'activeAlert',
+      render: (text) => <span style={{ color: 'black' }}>{text}</span>,
+    },
+    {
+      title: 'Time',
+      dataIndex: 'time',
+    },
+    {
+      title: 'Event Description',
+      dataIndex: 'eventDescription',
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      render: (status) => {
+        const color = status === 'Resolved' ? '#5C9D48' : '#D92D20'
+        return (
+          <span
+            style={{
+              color: color,
+            }}
+          >
+            {status}
+          </span>
+        )
+      },
+    },
+  ]
+
+  const activeAlertdata = [
+    {
+      key: '1',
+      activeAlert: 'Low panel voltage',
+      time: '11:58pm  2/01/2023',
+      eventDescription:
+        'Disconnect your solar panel from your PV system and Make sure your solar panel is getting sufficient light.',
+      status: 'Unresolved',
+    },
+    {
+      key: '2',
+      activeAlert: 'Maintenance overdue',
+      time: '11:58pm  2/01/2023',
+      eventDescription: 'Contact support',
+      status: 'Unresolved',
+    },
+    {
+      key: '3',
+      activeAlert: 'Low panel voltage',
+      time: '11:58pm  2/01/2023',
+      eventDescription:
+        'Check for corroded/loose connections, if none found contact support.',
+      status: 'Resolved',
+    },
+    {
+      key: '4',
+      activeAlert: 'Low panel voltage',
+      time: '11:58pm  2/01/2023',
+      eventDescription:
+        'Check for corroded/loose connections, if none found contact support.',
+      status: 'Unresolved',
+    },
+  ]
   return (
     <PageLayout>
-      <div className={classes.ActiveAlerts}>
+      <section className={classes.ActiveAlerts}>
         <section className={classes.ActiveAlerts__headerSection}>
           <PageBreadcrumb title="Active Alert" />
           <ShsCapacityDropdown />
@@ -42,7 +124,17 @@ const ActiveAlerts = () => {
             </div>
           </div>
         </section>
-      </div>
+        <section className={classes.ActiveAlerts__Table}>
+          <Table
+            title={title}
+            footer={footer}
+            columns={columns}
+            bordered={false}
+            dataSource={activeAlertdata}
+            pagination={false}
+          />
+        </section>
+      </section>
     </PageLayout>
   )
 }
