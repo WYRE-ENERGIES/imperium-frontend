ca290293b3c75ac8d9cb2ea9ce90d6941d5230f0
evie-adenoyin <ekong.emmanuel@lmu.edu.ng>
2023-02-19 10:37:38 +0100

    Admin Right Layout Design

--- src/components/Auth/Layout/RightLayout/RightLayout.jsx
+++ src/components/Auth/Layout/RightLayout/RightLayout.jsx
@@ -3,20 +3,33 @@ import React from 'react'
 import { BiEnvelope } from 'react-icons/bi'
 
 import classes from './RightLayout.module.scss'
-const RightLayout = ({ children }) => {
+const RightLayout = ({
+  children,
+  span = 12,
+  backgroundColor = 'rgba(240, 247, 237, 0.6)',
+}) => {
   return (
-    <Col span={12}>
-      <div className={classes.Layout}>
+    <Col
+      span={span}
+      style={{ backgroundColor: backgroundColor }}
+      className={classes.Layout}
+    >
+      <div>
         <Row justify="center" align={'middle'}>
           <div>{children}</div>
         </Row>
         <div className={classes.Layout__footer}>
-          <p>© imperium 2023</p>
-          <div className={classes.Layout__icon}>
-            <p>
-              <BiEnvelope size={18} />
-            </p>
-            <p>help@imperium.com</p>
+          <div className={classes.Layout__footerContact}>
+            {' '}
+            <div>
+              <p>© imperium 2023</p>
+            </div>
+            <div className={classes.Layout__icon}>
+              <p>
+                <BiEnvelope size={18} />
+              </p>
+              <p>help@imperium.com</p>
+            </div>
           </div>
         </div>
       </div>
