82f87ca571639735d0db097f36cee5a485c955f2
Solomon <solomon@combyne.com>
2023-02-22 07:30:31 +0100

    setup redux

---
+++ src/features/services/auth.services.js
@@ -0,0 +1,12 @@
+// all api calls for authentication goes here
+import axios from 'axios'
+
+const AUTH_URL = process.env.NEXT_PUBLIC_API_BASE_URL
+
+const registerUser = async () => {}
+
+const authServices = {
+  registerUser,
+}
+
+export default authServices
