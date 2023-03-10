b37d3acf4b8504b3aff08689bf72c05cbb93d6d2
evie-adenoyin <ekong.emmanuel@lmu.edu.ng>
2023-03-10 20:25:24 +0100

    user information display

--- src/pages/Customer/Account/AccountProfile.jsx
+++ src/pages/Customer/Account/AccountProfile.jsx
@@ -3,10 +3,11 @@ import { Row, Col } from 'antd'
 import classes from './Account.module.scss'
 import { message, Form, Upload } from 'antd'
 import uploadImg from '../../../assets/widget-icons/bussinessuploadIcon.svg'
+import { getItemFromLocalStorage } from '../../../utils/helpers'
 const AccountInfo = ({ AcctInfo, type }) => {
-  const { firstName, lastName, email } = AcctInfo
-  const firstNameInit = firstName[0]
-  const lastNameInit = lastName[0]
+  const { id, first_name, last_name, email, phone_number } =
+    getItemFromLocalStorage('userInfo')
+
   const [fileUpload, setFileUpload] = useState(false)
   const onFinish = (values) => {
     console.log('Success:', values)
@@ -95,8 +96,8 @@ const AccountInfo = ({ AcctInfo, type }) => {
           ) : (
             <div className={classes.AccountInfo__Init}>
               <p>
-                {firstNameInit}
-                {lastNameInit}
+                {first_name[0]}
+                {last_name[0]}
               </p>
             </div>
           )}
@@ -106,7 +107,7 @@ const AccountInfo = ({ AcctInfo, type }) => {
             {' '}
             <div>
               <p>
-                {firstName} {lastName}
+                {first_name} {last_name}
               </p>
               <p>{email}</p>
             </div>
