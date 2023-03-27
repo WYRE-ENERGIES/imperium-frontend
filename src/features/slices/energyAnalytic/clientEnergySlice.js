b799c9ed98df3a38769dc6e66cd5503a17035c5f
Solomon <solomon@combyne.com>
2023-03-27 05:00:33 +0100

    consume client energy api

--- src/features/slices/energyAnalytic/clientEnergySlice.js
+++ src/features/slices/energyAnalytic/clientEnergySlice.js
@@ -1,12 +1,14 @@
 import { apiSlice } from '../../api/apiSlice'
+import { getItemFromLocalStorage } from '../../../utils/helpers'
 
 const BASE_ENERGY_URL = '/imperium-client/energy/'
 
 export const energyApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getClientEnergyTableData: builder.query({
-      query: ({ page, search, filterBy }) => {
-        let url = `${BASE_ENERGY_URL}?page=${page}&order_by=${filterBy}`
+      query: ({ page, search, filterBy = 'yearly', deviceId }) => {
+        const clientId = getItemFromLocalStorage('current_client')
+        let url = `${BASE_ENERGY_URL}list-table/${clientId}/${deviceId}?page=${page}&order_by=${filterBy}`
         if (search) {
           url += `&search=${search}`
         }
@@ -14,7 +16,49 @@ export const energyApiSlice = apiSlice.injectEndpoints({
         return { url }
       },
     }),
+    getClientEnergyAnalytics: builder.query({
+      query: ({ deviceId, filterBy = 'yearly' }) => {
+        const clientId = getItemFromLocalStorage('current_client')
+        return {
+          url: `${BASE_ENERGY_URL}analytics/${clientId}/${deviceId}/?order_by=${filterBy}`,
+        }
+      },
+    }),
+    getClientEnergyStat: builder.query({
+      query: ({ deviceId, filterBy = 'yearly' }) => {
+        const clientId = getItemFromLocalStorage('current_client')
+        return {
+          url: `${BASE_ENERGY_URL}capacity-statistics/${clientId}/${deviceId}/?order_by=${filterBy}`,
+        }
+      },
+      transformResponse: (response) => {
+        const [res] = response
+        const energyConsumed = {
+          name: 'Energy Consumed',
+          data: [],
+        }
+        const energyGenerated = {
+          name: 'Energy Generated',
+          data: [],
+        }
+
+        Object.values(res).forEach((result) => {
+          const { energy_consumed, energy_generated } = result
+          energyConsumed.data.push(Math.round(energy_consumed) ?? 0)
+          energyGenerated.data.push(Math.round(energy_generated) ?? 0)
+        })
+
+        return {
+          energyConsumed,
+          energyGenerated,
+        }
+      },
+    }),
   }),
 })
 
-export const { useGetClientEnergyTableDataQuery } = energyApiSlice
+export const {
+  useGetClientEnergyTableDataQuery,
+  useGetClientEnergyAnalyticsQuery,
+  useGetClientEnergyStatQuery,
+} = energyApiSlice
