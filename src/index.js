d4928f48b2afa18d3535db9bf6e2579aef047e68
evie-adenoyin <ekong.emmanuel@lmu.edu.ng>
2023-05-22 14:36:32 +0100

    Google auth deleted

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './index.css'
-import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App'
 import { Provider } from 'react-redux'
 import React from 'react'
@@ -9,10 +8,8 @@ import { store } from './store'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="924325109646-o67mb2njlourq5jfscuqjepabcn1h5mj.apps.googleusercontent.com">
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </GoogleOAuthProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
