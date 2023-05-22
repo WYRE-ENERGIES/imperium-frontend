3cb2a51888613272ffc774802cada7b96bbb427b
evie-adenoyin <ekong.emmanuel@lmu.edu.ng>
2023-05-22 14:29:50 +0100

    google auth

--- src/components/Auth/Forms/Widgets/ThirdPartyAuth.jsx
+++ src/components/Auth/Forms/Widgets/ThirdPartyAuth.jsx
@@ -2,14 +2,8 @@ import { Divider } from 'antd'
 import React from 'react'
 import googleIcon from '../../../../../src/assets/Auth/google.svg'
 import classes from './Widget.module.scss'
-import { useGoogleLogin } from '@react-oauth/google'
 
 const ThirdPartyAuth = ({ signIn }) => {
-  // const handleGoogleAuth = useGoogleLogin({
-  //   onSuccess: (tokenResponse) => console.log(tokenResponse),
-  //   onError: (error) => console.log('Login Failed:', error),
-  //   redirect_uri: 'https://imperiumdev.wyreng.com/auth/google-oauth/',
-  // })
   return (
     <div className={classes.ThirdPartyAuth}>
       <a href="https://accounts.google.com/o/oauth2/auth?client_id=924325109646-o67mb2njlourq5jfscuqjepabcn1h5mj.apps.googleusercontent.com&redirect_uri=https://imperiumdev.wyreng.com/auth/google-oauth/&scope=https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email&response_type=code">
