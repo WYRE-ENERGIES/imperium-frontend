241b34b52b5f9909cb1b1dd7cdf33de0158f96a2
dtekluva <kboysreel@gmail.com>
2023-08-27 21:59:20 +0100

    UPDATE

--- src/components/SHSTable/SHSTable.jsx
+++ src/components/SHSTable/SHSTable.jsx
@@ -17,13 +17,13 @@ const columns = [
     title: 'Energy Consumed',
     key: 'energy_consumed',
     dataIndex: 'energy_consumed',
-    render: (value) => parseFloat(value.toLocaleString()).toFixed(1),
+    render: (value) => parseFloat(value.toLocaleString()),
   },
   {
     title: 'Energy Generated',
     key: 'energy_generated',
     dataIndex: 'energy_generated',
-    render: (value) => parseFloat(value.toLocaleString()).toFixed(1),
+    render: (value) => parseFloat(value.toLocaleString()),
   },
   {
     title: 'Location',
