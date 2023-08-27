5864e808915b8daf19f03d937758dbb780654960
dtekluva <kboysreel@gmail.com>
2023-08-27 22:27:25 +0100

    UPDATE[FIX TRUNCATION OF NUMBERS]

--- src/components/SHSTable/SHSTable.jsx
+++ src/components/SHSTable/SHSTable.jsx
@@ -17,13 +17,13 @@ const columns = [
     title: 'Energy Consumed',
     key: 'energy_consumed',
     dataIndex: 'energy_consumed',
-    render: (value) => 0 + value.toFixed(1),
+    render: (value) => (0 + value).toFixed(1),
   },
   {
     title: 'Energy Generated',
     key: 'energy_generated',
     dataIndex: 'energy_generated',
-    render: (value) => 0 + value.toFixed(1),
+    render: (value) => (0 + value).toFixed(1),
   },
   {
     title: 'Location',
