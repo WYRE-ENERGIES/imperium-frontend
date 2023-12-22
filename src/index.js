001d93c46fc950adad1699309da3352c558c4fdb
dtekluva <kboysreel@gmail.com>
2023-12-22 07:12:27 +0100

    UPDATE

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
-import './index.css'
-import App from './App'
-import { Provider } from 'react-redux'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { store } from './store'
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-)
-
 // import './index.css'
 // import App from './App'
 // import { Provider } from 'react-redux'
 // import React from 'react'
 // import ReactDOM from 'react-dom/client'
 // import { store } from './store'
-// import InactivityTimeout from './inactivityTimeout' // Adjust the path
 
 // const root = ReactDOM.createRoot(document.getElementById('root'))
-
 // root.render(
 //   <React.StrictMode>
 //     <Provider store={store}>
-//       <InactivityTimeout timeoutInSeconds={10}>
-//         <App />
-//       </InactivityTimeout>
+//       <App />
 //     </Provider>
 //   </React.StrictMode>,
 // )
+
+import './index.css'
+import App from './App'
+import { Provider } from 'react-redux'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { store } from './store'
+import InactivityTimeout from './inactivityTimeout' // Adjust the path
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <InactivityTimeout timeoutInSeconds={100}>
+        <App />
+      </InactivityTimeout>
+    </Provider>
+  </React.StrictMode>,
+)
