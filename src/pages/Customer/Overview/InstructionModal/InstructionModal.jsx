0877ecb3d3046e0a8e6ead7ea5dfd6e7d4b47332
dtekluva <kboysreel@gmail.com>
2023-12-22 06:08:40 +0100

    MINOR UPDATE

--- src/pages/Customer/Overview/InstructionModal/InstructionModal.jsx
+++ src/pages/Customer/Overview/InstructionModal/InstructionModal.jsx
@@ -1,40 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { MdLogout } from 'react-icons/md';
-import { Modal } from 'antd';
-import { logOutUser } from '../../../../features/slices/auth/authSlice';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { CloseOutlined } from '@ant-design/icons';
-import classes from './InstructionModal.module.scss';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { MdLogout } from 'react-icons/md'
+import { Modal } from 'antd'
+import { logOutUser } from '../../../../features/slices/auth/authSlice'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import classes from './InstructionModal.module.scss'
+import { CloseOutlined } from '@ant-design/icons'
 
 const InstructionModal = ({ open, isAdmin, setOpen }) => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const handleEmailClick = (event) => {
-    if (navigator.share) {
-      navigator.share({
-        title: 'Contact Support',
-        text: 'Compose an email',
-        url: 'mailto:imperiumbysterling@gmail.com',
-      })
-      .then(() => console.log('Share dialog opened'))
-      .catch((error) => console.error('Error opening share dialog:', error));
-    } else {
-      window.location.href = event.target.href;
-    }
-
-    // Prevent the default click action
-    event.preventDefault();
-  };
-
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
   const onLogout = () => {
-    const navigateTo = isAdmin ? '/admin/sign-in' : '/';
-    dispatch(logOutUser());
-    navigate(navigateTo);
-  };
-
+    const navigateTo = isAdmin ? '/admin/sign-in' : '/'
+    dispatch(logOutUser())
+    navigate(navigateTo)
+  }
   return (
     <Modal centered open={open} footer={null} className={classes.ModalMain}>
       <div className={classes.InstructionModal}>
@@ -46,7 +27,7 @@ const InstructionModal = ({ open, isAdmin, setOpen }) => {
         </div>
         <h1>Welcome to Imperium Solar house system</h1>
         <h4>
-          You do not have an active SHS connected to this account. Would you
+          You do not have an active SHS connected to this account. How would you
           like to set up an SHS and enjoy all Imperium features?
           <div>You can contact support via email to imperiumbysterling@gmail.com</div>
         </h4>
@@ -64,7 +45,6 @@ const InstructionModal = ({ open, isAdmin, setOpen }) => {
             rel="noreferrer"
             target="_blank"
             className={classes.InstructionModal__buttonBottom}
-            onClick={handleEmailClick}
           >
             Contact Support
           </a>
@@ -78,7 +58,7 @@ const InstructionModal = ({ open, isAdmin, setOpen }) => {
         </div>
       </div>
     </Modal>
-  );
-};
+  )
+}
 
-export default InstructionModal;
+export default InstructionModal
